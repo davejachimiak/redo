816b567e5ad4ed519f50d97d184adb415e9203cc
Dave Jachimiak <dave.jachimiak@gmail.com>
2014-02-17 15:18:21 -0500

    Extract redis func

--- src/Redo.hs
+++ src/Redo.hs
@@ -4,10 +4,13 @@ import Data.ByteString.Char8
 
 main = do
     (_:task:_) <- getArgs
+
+    add task
+
+add :: String -> IO (Either Reply Integer)
+add task = withRedis $ rpush (pack "redo") [(pack task)]
+
+withRedis :: Redis a -> IO a
+withRedis f = do
     conn <- connect defaultConnectInfo
-
-    add conn task
-
-add :: Connection -> String -> IO (Either Reply Integer)
-add conn task = runRedis conn $ do
-    rpush (pack "redo") [(pack task)]
+    runRedis conn f
