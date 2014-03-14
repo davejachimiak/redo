41d247e95a794c8cd3cd09e714d0168ab4fa4dbf
Dave Jachimiak <dave.jachimiak@gmail.com>
2014-03-14 09:24:35 -0400

    Use result class for adding tasks

--- src/Redo.hs
+++ src/Redo.hs
@@ -8,6 +8,15 @@ import Data.Monoid
 type Tasks = [String]
 type Args = [String]
 
+data Add e r = Add (Either e r)
+
+class Result r where
+    handleResult :: r -> ByteString
+
+instance (Show e) => Result (Add e r) where
+    handleResult (Add (Right _)) = B.pack "Tasks added"
+    handleResult (Add (Left e)) = B.pack "Error: " `mappend` B.pack (show e)
+
 main = do
     args <- getArgs
     response <- run args
@@ -18,18 +27,14 @@ run ("add":xs) = add xs
 run ("list":_) = list
 
 add :: [String] -> IO ByteString
-add tasks = do
-    result <- withRedis $ rpush namespace $ map B.pack tasks
-    case result of
-        Right _ -> return $ B.pack "Tasks added"
-        Left reply -> return $ handleError reply
+add tasks = handleResult <$> Add <$> withRedis (rpush namespace $ map B.pack tasks)
 
 list :: IO ByteString
 list = do
     let numericizeTask n t = mconcat [B.pack (show n), B.pack " -- ", t]
     let parseResult (Right ts) = B.unlines $ zipWith numericizeTask [1..] ts
         parseResult (Left reply) = handleError reply
-    parseResult <$> (withRedis $ lrange namespace 0 (-1))
+    parseResult <$> withRedis (lrange namespace 0 (-1))
 
 handleError :: Reply -> ByteString
 handleError = B.pack . show
