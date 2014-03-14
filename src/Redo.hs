6f52921a716fdccc0aac33bb10dc25de4fc008b2
Dave Jachimiak <dave.jachimiak@gmail.com>
2014-03-13 21:03:58 -0400

    Refactor

--- src/Redo.hs
+++ src/Redo.hs
@@ -4,14 +4,22 @@ import qualified Data.ByteString.Char8 as B
 import Data.ByteString ( ByteString )
 import Control.Applicative
 
-main = do
-    args <- map B.pack <$> getArgs
-    result <- add $ tail args
-    B.putStrLn result
+data Command = Add Tasks deriving Show
 
-add :: [ByteString] -> IO ByteString
+type Tasks = [String]
+type Args = [String]
+
+main = do
+    args <- getArgs
+    response <- run args
+    B.putStrLn response
+
+run :: Args -> IO ByteString
+run ("add":xs) = add xs
+
+add :: [String] -> IO ByteString
 add tasks = do
-    result <- withRedis $ rpush namespace tasks
+    result <- withRedis $ rpush namespace $ map B.pack tasks
     case result of
         Right _ -> return $ B.pack "Tasks added"
         Left reply -> return $ B.pack $ show reply
