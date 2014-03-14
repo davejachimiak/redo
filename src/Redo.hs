717b8d88fc9b326a5e6cca81ea5024eb6ae4b67f
Dave Jachimiak <dave.jachimiak@gmail.com>
2014-03-14 19:15:03 -0400

    Handle list with instance

--- src/Redo.hs
+++ src/Redo.hs
@@ -8,14 +8,19 @@ import Data.Monoid
 type Tasks = [String]
 type Args = [String]
 
-data Add e r = Add (Either e r)
+data Add e r = Add (Either e Integer)
+data ListResult e r = ListResult (Either e [ByteString]) deriving Show
 
 class Result r where
     handleResult :: r -> ByteString
 
 instance (Show e) => Result (Add e r) where
     handleResult (Add (Right _)) = B.pack "Tasks added"
-    handleResult (Add (Left e)) = B.pack "Error: " `mappend` B.pack (show e)
+    handleResult (Add (Left e)) = B.pack (show e)
+
+instance (Show e) => Result (ListResult e r) where
+    handleResult (ListResult (Right ts)) = B.unlines $ zipWith numericizeTask [1..] ts
+    handleResult (ListResult (Left e)) = B.pack (show e)
 
 main = do
     args <- getArgs
@@ -30,14 +35,10 @@ add :: [String] -> IO ByteString
 add tasks = handleResult <$> Add <$> withRedis (rpush namespace $ map B.pack tasks)
 
 list :: IO ByteString
-list = do
-    let numericizeTask n t = mconcat [B.pack (show n), B.pack " -- ", t]
-    let parseResult (Right ts) = B.unlines $ zipWith numericizeTask [1..] ts
-        parseResult (Left reply) = handleError reply
-    parseResult <$> withRedis (lrange namespace 0 (-1))
+list = handleResult <$> ListResult <$> withRedis (lrange namespace 0 (-1))
 
-handleError :: Reply -> ByteString
-handleError = B.pack . show
+numericizeTask :: Integer -> ByteString -> ByteString
+numericizeTask n t = mconcat [B.pack (show n), B.pack " -- ", t]
 
 withRedis :: Redis a -> IO a
 withRedis action = do
