443ed28d853f650a92b79eee45ce9e83e37aaa0f
Dave Jachimiak <dave.jachimiak@gmail.com>
2014-03-13 21:29:28 -0400

    List

--- src/Redo.hs
+++ src/Redo.hs
@@ -16,12 +16,21 @@ main = do
 
 run :: Args -> IO ByteString
 run ("add":xs) = add xs
+run ("list":_) = list
 
 add :: [String] -> IO ByteString
 add tasks = do
     result <- withRedis $ rpush namespace $ map B.pack tasks
     case result of
         Right _ -> return $ B.pack "Tasks added"
+        Left reply -> return $ B.pack $ show reply
+
+list :: IO ByteString
+list = do
+    let numericizeTask n t = B.pack (show n) `B.append` B.pack " -- " `B.append` t
+    result <- withRedis $ lrange namespace 0 (-1)
+    case result of
+        Right ts -> return $ B.unlines $ zipWith numericizeTask [1..] ts
         Left reply -> return $ B.pack $ show reply
 
 withRedis :: Redis a -> IO a
